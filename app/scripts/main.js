83151eb50c3479efcfec27ee61d820df14e1e794
Franco Zanki <fazanki@gmail.com>
2015-05-01 22:26:18 +0100

    reorganising

---
+++ app/scripts/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery'
+    },
+    shim: {
+       
+
+    }
+});
+
+require(['views/app', 'jquery'], function(AppView){
+    
+
+});
